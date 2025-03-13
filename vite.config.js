d2133e2494912ee1d209fe47a15e75a08b1a0fe0
Rainer Simon <rainer@rainersimon.io>
2025-03-13 12:19:13 +0100

    Build

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,9 @@ export default defineConfig({
   build: {
     sourcemap: true,
     lib: {
-      formats: ['es']
+      entry: './src/index.ts',
+      formats: ['es'],
+      fileName: 'index'
     }
   }
 });
